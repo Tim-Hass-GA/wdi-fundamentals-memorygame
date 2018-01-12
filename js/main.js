d731fc42a09d95d3e02161e7e296fc405884f9a2
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-01-12 11:37:14 -0800

    created game logic unit 9

--- js/main.js
+++ js/main.js
@@ -1,30 +1,28 @@
-// console.log("Up and running!");
-
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
-
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardFour);
+// tim hass
+// 2018.1.12
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne;
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo;
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
+function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try Again.")
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try Again.")
 	}
 }
+
+function flipCard(cardId){
+
+	console.log("User flipped " + cardId);
+	var currentCard = cards[cardId];
+	cardsInPlay.push(currentCard);
+	console.log("currentCard = " + currentCard);
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);

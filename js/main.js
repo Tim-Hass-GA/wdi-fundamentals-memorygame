a6c187f1a3c1ebc72516d6a472f162c1d11d820f
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-01-12 13:32:43 -0800

    created game logic unit 10

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 // tim hass
 // 2018.1.12
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -13,11 +34,10 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-
-	console.log("User flipped " + cardId);
-	var currentCard = cards[cardId];
-	cardsInPlay.push(currentCard);
-	console.log("currentCard = " + currentCard);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2){
 		checkForMatch();
